720e294d98d3859fa79a987e8c46125460a0417f
Lucas <lucas.fache0@gmail.com>
2026-08-16 02:28:09 +0200

    fix: db config pour Railway

--- src/db.js
+++ src/db.js
@@ -1,13 +1,19 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: "postgres",
-  host: "teyvat-db",
-  database: "teyvat",
-  password: "postgres",
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: "postgres",
+        host: "teyvat-db",
+        database: "teyvat",
+        password: "postgres",
+        port: 5432,
+      }
+);
 
 export default pool;
-
--- src/seed.js
+++ src/seed.js
@@ -2,25 +2,30 @@ import fetch from "node-fetch";
 import pkg from "pg";
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: "postgres",
-  host: "teyvat-db",
-  database: "teyvat",
-  password: "postgres",
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: "postgres",
+        host: "teyvat-db",
+        database: "teyvat",
+        password: "postgres",
+        port: 5432,
+      }
+);
 
 async function seed() {
   const res = await fetch(
     "https://genshin-center.com/_next/data/RGFMLahh0T1b4znjvJakh/fr/characters.json"
   );
   const data = await res.json();
-
   const characters = Object.values(data.pageProps.characters);
 
   for (const c of characters) {
     const slug = c.img.toLowerCase();
-
     await pool.query(
       `INSERT INTO characters (name, slug, element, weapon, rarity, image)
        VALUES ($1, $2, $3, $4, $5, $6)
